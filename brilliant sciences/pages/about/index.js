0a666330752018bece522c191a35fd2b36b811aa
inezaodon <132347184+inezaodon@users.noreply.github.com>
2023-11-11 18:47:02 +0200

    Delete brilliant sciences directory

--- brilliant sciences/pages/about/index.js
+++
@@ -1,6 +0,0 @@
-export default () => {
-    return (
-      <div>In The About</div>
-    )
-  }
-  
--- brilliant sciences/redux/actions/index.js
+++
@@ -1,6 +0,0 @@
-export const SET = (key, value) => {
-    return {
-        type: key,
-        payload: value
-    }
-}
