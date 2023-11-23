aeba224e5440605894ac8467af59daecb0ff9d94
inezaodon <132347184+inezaodon@users.noreply.github.com>
2023-11-23 09:27:29 +0200

    Update firebase.js

--- helpers/firebase.js
+++ helpers/firebase.js
@@ -5,12 +5,12 @@ import "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+   authDomain: "brilliant-sciences.firebaseapp.com",
+  projectId: "brilliant-sciences",
+  storageBucket: "brilliant-sciences.appspot.com",
+  messagingSenderId: "811825813849",
+  appId: "1:811825813849:web:f2ac7de4c861ee7cd15448",
+  measurementId: "G-L87TNW9W4B"
 }
 
 // // Initialize Firebase
